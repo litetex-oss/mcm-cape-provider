b397122815f1c3fa3fd13ba1a696bf13fa12fcdd
litetex <40789489+litetex@users.noreply.github.com>
2026-05-15 19:40:33 +0200

    Update to 26.2 (snap7)

--- src/main/java/net/litetex/capes/menu/provider/preview/render/PlayerDisplayGuiElementRenderer.java
+++ src/main/java/net/litetex/capes/menu/provider/preview/render/PlayerDisplayGuiElementRenderer.java
@@ -8,28 +8,26 @@ import org.joml.Matrix4fStack;
 import com.mojang.blaze3d.platform.Lighting;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
-import com.mojang.blaze3d.vertex.VertexConsumer;
 import com.mojang.math.Axis;
 
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.render.pip.PictureInPictureRenderer;
-import net.minecraft.client.model.Model;
-import net.minecraft.client.model.geom.ModelPart;
-import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.SubmitNodeCollector;
 import net.minecraft.client.renderer.rendertype.RenderTypes;
 import net.minecraft.client.renderer.texture.OverlayTexture;
 import net.minecraft.core.ClientAsset;
 import net.minecraft.resources.Identifier;
 
 
+// See also: GuiSkinRenderer
 @SuppressWarnings("checkstyle:MagicNumber")
 public class PlayerDisplayGuiElementRenderer extends PictureInPictureRenderer<PlayerDisplayGuiElementRenderState>
 {
 	private static final int LIGHT = 0xF000F0;
 	
-	public PlayerDisplayGuiElementRenderer(final MultiBufferSource.BufferSource immediate)
+	public PlayerDisplayGuiElementRenderer()
 	{
-		super(immediate);
+		super();
 	}
 	
 	@Override
@@ -40,69 +38,85 @@ public class PlayerDisplayGuiElementRenderer extends PictureInPictureRenderer<Pl
 	
 	@Override
 	protected void renderToTexture(
-		final PlayerDisplayGuiElementRenderState state,
-		final PoseStack matrixStack)
+		final PlayerDisplayGuiElementRenderState skinState,
+		final PoseStack modelStack,
+		final SubmitNodeCollector submitNodeCollector)
 	{
-		Minecraft.getInstance().gameRenderer.getLighting()
-			.setupFor(Lighting.Entry.PLAYER_SKIN);
+		Minecraft.getInstance().gameRenderer.lighting().setupFor(Lighting.Entry.PLAYER_SKIN);
 		
 		final int windowScaleFactor = Minecraft.getInstance().getWindow().getGuiScale();
 		
-		final Matrix4fStack matrix4fStack = RenderSystem.getModelViewStack();
-		matrix4fStack.pushMatrix();
-		final float f = state.scale() * windowScaleFactor;
-		matrix4fStack.rotateAround(
-			Axis.XP.rotationDegrees(state.xRotation()),
+		final Matrix4fStack viewStack = RenderSystem.getModelViewStack();
+		viewStack.pushMatrix();
+		final float scale = skinState.scale() * windowScaleFactor;
+		// MC-308168 Rotation is not working
+		viewStack.rotateAround(
+			Axis.XP.rotationDegrees(skinState.xRotation()),
 			0.0F,
-			f * -state.yPivot(),
+			scale * -skinState.yPivot(),
 			0.0F
 		);
-		matrixStack.mulPose(Axis.YP.rotationDegrees(-state.yRotation()));
-		matrixStack.translate(0.0F, -1.6010001F, 0.0F);
+		modelStack.mulPose(Axis.YP.rotationDegrees(-skinState.yRotation()));
+		modelStack.translate(0.0F, -1.6010001F, 0.0F);
 		
-		this.renderParts(state.payload(), state.models(), matrixStack);
+		this.renderParts(skinState.payload(), skinState.models(), modelStack, submitNodeCollector);
 		
-		this.bufferSource.endBatch();
-		matrix4fStack.popMatrix();
+		viewStack.popMatrix();
 	}
 	
 	protected void renderParts(
 		final PlayerDisplayGuiPayload payload,
 		final PlayerDisplayGuiModels models,
-		final PoseStack matrixStack)
+		final PoseStack modelStack,
+		final SubmitNodeCollector submitNodeCollector)
 	{
 		if(payload.bodyTexture() != null)
 		{
-			this.render(
+			submitNodeCollector.submitModel(
 				models.player(),
-				matrixStack,
-				this.bufferSource.getBuffer(models.player().renderType(payload.bodyTexture().texturePath())));
+				models.state(),
+				modelStack,
+				payload.bodyTexture().texturePath(),
+				LIGHT,
+				OverlayTexture.NO_OVERLAY,
+				0,
+				null
+			);
 		}
 		
 		if(payload.elytraTextureSupplier() != null)
 		{
 			this.extractFromSupplierAndRender(
-				payload.elytraTextureSupplier(), matrixStack, id ->
+				payload.elytraTextureSupplier(), id ->
 				{
-					matrixStack.translate(0.0f, 0.0f, 0.125f);
+					modelStack.translate(0.0f, 0.0f, 0.125f);
 					
-					this.render(
+					submitNodeCollector.submitModel(
 						models.elytra(),
-						matrixStack,
-						this.bufferSource.getBuffer(RenderTypes.armorCutoutNoCull(id)));
+						models.state(),
+						modelStack,
+						RenderTypes.armorCutoutNoCull(id),
+						LIGHT,
+						OverlayTexture.NO_OVERLAY,
+						0,
+						null
+					);
 				});
 		}
 		else if(payload.capeTextureSupplier() != null)
 		{
 			this.extractFromSupplierAndRender(
-				payload.capeTextureSupplier(), matrixStack, id ->
+				payload.capeTextureSupplier(), id ->
 				{
-					matrixStack.mulPose(Axis.XP.rotationDegrees(6.0f));
+					modelStack.mulPose(Axis.XP.rotationDegrees(6.0f));
 					
-					this.render(
+					submitNodeCollector.submitModelPart(
 						models.cape().getChild("body").getChild("cape"),
-						matrixStack,
-						this.bufferSource.getBuffer(RenderTypes.armorCutoutNoCull(id))
+						modelStack,
+						RenderTypes.armorCutoutNoCull(id),
+						LIGHT,
+						OverlayTexture.NO_OVERLAY,
+						null
 					);
 				});
 		}
@@ -110,7 +124,6 @@ public class PlayerDisplayGuiElementRenderer extends PictureInPictureRenderer<Pl
 	
 	protected void extractFromSupplierAndRender(
 		final Supplier<ClientAsset.Texture> supplier,
-		final PoseStack matrixStack,
 		final Consumer<Identifier> renderer)
 	{
 		final ClientAsset.Texture textureAsset = supplier.get();
@@ -125,19 +138,7 @@ public class PlayerDisplayGuiElementRenderer extends PictureInPictureRenderer<Pl
 			return;
 		}
 		
-		matrixStack.pushPose();
 		renderer.accept(id);
-		matrixStack.popPose();
-	}
-	
-	protected void render(final Model<?> model, final PoseStack stack, final VertexConsumer c)
-	{
-		model.renderToBuffer(stack, c, LIGHT, OverlayTexture.NO_OVERLAY);
-	}
-	
-	protected void render(final ModelPart modelPart, final PoseStack stack, final VertexConsumer c)
-	{
-		modelPart.render(stack, c, LIGHT, OverlayTexture.NO_OVERLAY);
 	}
 	
 	@Override
